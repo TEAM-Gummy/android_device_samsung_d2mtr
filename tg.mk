267edc5df5e8ef9aa1924dcc1a5e5b06fce09290
hedwig34 <hedwig34@gmail.com>
2013-10-20 12:15:25 -0400

    Initial changes for Gummy.

--- tg.mk
+++ tg.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/Gummy/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2mtr \
@@ -15,6 +15,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMF2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMF2 release-keys"
 
-PRODUCT_NAME := cm_d2mtr
+PRODUCT_NAME := tg_d2mtr
 PRODUCT_DEVICE := d2mtr
 
